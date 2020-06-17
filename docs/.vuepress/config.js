3363b85de0513da72d45c073d6709398336d1f3e
grafikri <grafikri@gmail.com>
2020-06-17 21:45:53 +0300

    docs files added

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+// .vuepress/config.js
+module.exports = {
+  title: 'Vue Line Clamp',
+  description: '',
+  themeConfig: {
+    sidebarDepth: 2,
+    nav: [
+      { text: 'GitHub', link: '#' }
+    ],
+    sidebar: [
+      {
+        title: 'Guide',
+        path: '/'
+      }
+
+    ]
+  }
+}
